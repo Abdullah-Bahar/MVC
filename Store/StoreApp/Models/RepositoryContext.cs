b5dacc71c898a33404930b7b3f11e87e7695d9f5
Abdullah Bahar <abdullahbahar007@gmail.com>
2025-12-17 11:44:07 +0300

    ConnectionString değiştirildi. RepositoryContext dosyasında override işlemi yapıldı. Migrationlar işlemleri yapıldı.

--- Store/StoreApp/Models/RepositoryContext.cs
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -7,8 +7,50 @@ public class RepositoryContext : DbContext
 	public RepositoryContext(DbContextOptions<RepositoryContext> options)
 		: base(options)
 	{
-		
+
 	}
 
 	public DbSet<Product> Products { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		// Model inşa edilirken model üzerinde veri yoksa aşağıdaki verileri ekler,
+		// eğer veri varsa eklemez.
+		// bu yazıldıktan sonra tekrar migration atılması gerekir.
+		modelBuilder.Entity<Product>()
+			.HasData(
+				new Product()
+				{
+					ProductId = 1,
+					ProductName = "Computer",
+					Price = 17_000
+				},
+				new Product()
+				{
+					ProductId = 2,
+					ProductName = "Keyboard",
+					Price = 1_000
+				},
+				new Product()
+				{
+					ProductId = 3,
+					ProductName = "Mouse",
+					Price = 500
+				},
+				new Product()
+				{
+					ProductId = 4,
+					ProductName = "Monitor",
+					Price = 10_000
+				},
+				new Product()
+				{
+					ProductId = 5,
+					ProductName = "Deck",
+					Price = 2_000
+				}
+			);
+	}
 }
