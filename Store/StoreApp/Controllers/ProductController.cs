57cd14200da5a7263b119f762950ba94682e5d7e
Abdullah Bahar <abdullahbahar007@gmail.com>
2025-12-17 12:14:03 +0300

    ProductController içerisinde RepositoryContext, constructor üzerinden Dependency Injection (DI) ile alınacak şekilde düzenlendi.
    Manuel DbContext oluşturma kaldırıldı ve Program.cs tarafında yapılan servis kaydı aktif olarak kullanılmaya başlandı.

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -6,32 +6,15 @@ namespace StoreApp.Controllers;
 
 public class ProductController : Controller
 {
+	private readonly RepositoryContext _context;
+
+	public ProductController(RepositoryContext context)
+	{
+		_context = context;
+	}
+
 	public IEnumerable<Product> Index()
 	{
-		// Propgram.cs dosyasındaki DbContext için yapılan servis kaydı
-		// aşağıda BAYPASS edilmiştir
-
-		// Bu metotta DbContext Dependency Injection (DI) üzerinden ALINMIYOR
-		// RepositoryContext (DbContext) manuel olarak oluşturuluyor
-		var context = new RepositoryContext(
-
-			// DbContextOptionsBuilder:
-			// RepositoryContext'in hangi ayarlarla çalışacağını tanımlar
-			// (hangi veritabanı, hangi provider, hangi connection string vb.)
-			new DbContextOptionsBuilder<RepositoryContext>()
-
-				// EF Core'a bu DbContext'in SQLite kullanacağını bildirir
-				// Buradaki connection string SABİT (hard-coded) olarak verilmiştir
-				// DI + Configuration yapısı kullanılmamaktadır
-				.UseSqlite("Data Source = C:\\sqlite3\\db\\ProductDb.db")
-
-				// Options:
-				// Oluşturulan tüm ayarların RepositoryContext'e verilmesini sağlar
-				.Options
-		);
-
-		// DbContext içindeki DbSet<Product>
-		// Yani veritabanındaki Products tablosu
-		return context.Products;
+		return _context.Products;
 	}
 }
